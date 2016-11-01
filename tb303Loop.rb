4e16bca26ebb3c5d57be85d14505ea1620f9d94b
juanpickselov@sbcglobal.net <juanpickselov@sbcglobal.net>
2016-11-01 00:02:39 -0400

    prophet loop tinkering

---
+++ tb303Loop.rb
@@ -0,0 +1,9 @@
+use_synth :tb303
+with_fx :echo, room: 1 do
+  live_loop :space_scanner do
+    play :e3, cutoff: 100, release: 8, attack: 6, cutoff_attack: 6, cutoff_release: 4
+    play :g3, cutoff: 100, release: 8, attack: 6, cutoff_attack: 6, cutoff_release: 4
+    play :c3, cutoff: 100, release: 8, attack: 6, cutoff_attack: 6, cutoff_release: 4
+    sleep 8
+  end
+end
