66d3cffa3779da1352b498771a1b2f6d765e9347
juanpickselov@sbcglobal.net <juanpickselov@sbcglobal.net>
2016-10-31 23:31:14 -0400

    more sncpi fun

---
+++ seaNoise002.rb
@@ -0,0 +1,7 @@
+use_synth :noise
+with_fx :reverb, room: 1 do
+  live_loop :space_scanner do
+    play :e3, cutoff: 100, release: 7, attack: 3, cutoff_attack: 6, cutoff_release: 6
+    sleep 8
+  end
+end
