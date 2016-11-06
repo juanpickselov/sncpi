bbb84f5c34a940c9989c1587a1927daffae61373
juanpickselov@sbcglobal.net <juanpickselov@sbcglobal.net>
2016-11-06 10:52:26 -0500

    If Doctor Who played Mary had a little lamb

---
+++ mhallwho.rb
@@ -0,0 +1,58 @@
+#a basic song which now needs to take away the repeated code
+with_bpm 120 do
+  with_fx :wobble do
+    with_synth :pluck do
+      play :E3
+      sleep 0.5
+      play :D3
+      sleep 0.5
+      play :C3
+      sleep 0.5
+      play :D3
+      sleep 0.5
+      play :E3
+      sleep 0.5
+      play :E3
+      sleep 0.5
+      play :E3
+      sleep 1
+      play :D3
+      sleep 0.5
+      play :D3
+      sleep 0.5
+      play :D3
+      sleep 1
+      play :E3
+      sleep 0.5
+      play :G3
+      sleep 0.5
+      play :G3
+      sleep 1
+      play :E3
+      sleep 0.5
+      play :D3
+      sleep 0.5
+      play :C3
+      sleep 0.5
+      play :D3
+      sleep 0.5
+      play :E3
+      sleep 0.5
+      play :E3
+      sleep 0.5
+      play :E3
+      sleep 0.5
+      play :E3
+      sleep 0.5
+      play :D3
+      sleep 0.5
+      play :D3
+      sleep 0.5
+      play :E3
+      sleep 0.5
+      play :D3
+      sleep 0.5
+      play :C3
+    end
+  end
+end
