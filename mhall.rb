a55171a495df2d2384c58dc67ffb2bb6b48b86ba
juanpickselov@sbcglobal.net <juanpickselov@sbcglobal.net>
2016-11-06 18:04:35 -0500

    removed repeated chunks and will refactor the song more later

--- mhall.rb
+++ mhall.rb
@@ -1,58 +1,61 @@
 #a basic song which now needs to take away the repeated code
-with_bpm 120 do
-  with_fx :slicer do
-    with_synth :pluck do
-      play :E3
-      sleep 0.5
-      play :D3
-      sleep 0.5
-      play :C3
-      sleep 0.5
-      play :D3
-      sleep 0.5
-      play :E3
-      sleep 0.5
-      play :E3
-      sleep 0.5
-      play :E3
-      sleep 1
-      play :D3
-      sleep 0.5
-      play :D3
-      sleep 0.5
-      play :D3
-      sleep 1
-      play :E3
-      sleep 0.5
-      play :G3
-      sleep 0.5
-      play :G3
-      sleep 1
-      play :E3
-      sleep 0.5
-      play :D3
-      sleep 0.5
-      play :C3
-      sleep 0.5
-      play :D3
-      sleep 0.5
-      play :E3
-      sleep 0.5
-      play :E3
-      sleep 0.5
-      play :E3
-      sleep 0.5
-      play :E3
-      sleep 0.5
-      play :D3
-      sleep 0.5
-      play :D3
-      sleep 0.5
-      play :E3
-      sleep 0.5
-      play :D3
-      sleep 0.5
-      play :C3
+define :mhalrpt do
+  play :E3
+  sleep 0.5
+  play :D3
+  sleep 0.5
+  play :C3
+  sleep 0.5
+  play :D3
+  sleep 0.5
+  play :E3
+  sleep 0.5
+  play :E3
+  sleep 0.5
+  play :E3
+  sleep 0.5
+end
+
+define :ll do
+  play :D3
+  sleep 0.5
+  play :D3
+  sleep 0.5
+  play :D3
+  sleep 1
+  play :E3
+  sleep 0.5
+  play :G3
+  sleep 0.5
+  play :G3
+  sleep 1
+end
+
+define :fwas do
+  play :E3
+  sleep 0.5
+  play :D3
+  sleep 0.5
+  play :D3
+  sleep 0.5
+  play :E3
+  sleep 0.5
+  play :D3
+  sleep 0.5
+  play :C3
+end
+
+2.times do
+  with_bpm 96 do
+    with_fx :flanger do
+      with_synth :pluck do
+        mhalrpt
+        sleep 0.5
+        ll
+        mhalrpt
+        fwas
+      end
     end
   end
+  sleep 1
 end
