7809c53b1417ac245635c51955b85eab47af4f38
juanpickselov@sbcglobal.net <juanpickselov@sbcglobal.net>
2016-11-06 10:34:37 -0500

    notes using synth

---
+++ mhall.rb
@@ -0,0 +1,57 @@
+with_bpm 96 do
+  with_fx :flanger do
+    with_synth :pluck do
+      play :E3
+      sleep 0.5
+      play :D3
+      sleep 0.5
+      play :C3
+      sleep 0.5
+      play :D3
+      sleep 0.5
+      play :E3
+      sleep 0.5
+      play :E3
+      sleep 0.5
+      play :E3
+      sleep 1
+      play :D3
+      sleep 0.5
+      play :D3
+      sleep 0.5
+      play :D3
+      sleep 1
+      play :E3
+      sleep 0.5
+      play :G3
+      sleep 0.5
+      play :G3
+      sleep 1
+      play :E3
+      sleep 0.5
+      play :D3
+      sleep 0.5
+      play :C3
+      sleep 0.5
+      play :D3
+      sleep 0.5
+      play :E3
+      sleep 0.5
+      play :E3
+      sleep 0.5
+      play :E3
+      sleep 0.5
+      play :E3
+      sleep 0.5
+      play :D3
+      sleep 0.5
+      play :D3
+      sleep 0.5
+      play :E3
+      sleep 0.5
+      play :D3
+      sleep 0.5
+      play :C3
+    end
+  end
+end
