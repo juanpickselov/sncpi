1ec57850cdffda1968815c80f5d50da712dc55cf
juanpickselov@sbcglobal.net <juanpickselov@sbcglobal.net>
2016-11-06 19:57:40 -0500

    fun with slide and beat stretch

--- letItSlide.rb
+++ letItSlide.rb
@@ -1,4 +1,24 @@
 #sample :guit_e_slide
+2.times do
+  with_bpm 80 do
+    with_fx :flanger do
+      sample :guit_e_slide, beat_stretch: 14
+      sleep 0.6
+      sample :guit_e_slide, beat_stretch: 12
+      sleep 0.6
+      sample :guit_e_slide, beat_stretch: 10
+      sleep 0.6
+      sample :guit_e_slide, beat_stretch: 8
+      sleep 0.6
+      sample :guit_e_slide, beat_stretch: 10
+      sleep 0.6
+      sample :guit_e_slide, beat_stretch: 12
+      sleep 0.6
+    end
+  end
+end
+
+#sample :guit_e_slide
 #3.times do
 with_bpm 80 do
   sample :guit_e_slide, beat_stretch: 16
