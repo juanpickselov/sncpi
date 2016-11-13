01e02a80e1055036f2c852d1772b980dd7022843
juanpickselov@sbcglobal.net <juanpickselov@sbcglobal.net>
2016-11-13 15:13:15 -0500

    bumblebee flight is not my code

---
+++ bumblebeeFlight.rb
@@ -0,0 +1,1268 @@
+# flight of the bumblebee RasPi.TV style (or lack thereof)
+# http://raspi.tv/?p=5216
+
+# first some rythms
+speed = 0.45  #set overall speed
+breve = 8 * speed
+semibreve = 4 * speed
+minim = 2 * speed
+crotchet = 1 * speed
+quaver = 0.5 * speed
+semi = 0.25 * speed
+demi = 0.125 * speed
+hemi = 0.0625 * speed
+
+
+play 93
+sleep semi
+play 92
+sleep semi
+play 91
+sleep semi
+play 90
+sleep semi
+play 91
+sleep semi
+play 90
+sleep semi
+play 89
+sleep semi
+play 88
+sleep semi
+
+play 89
+sleep semi
+play 88
+sleep semi
+play 87
+sleep semi
+play 86
+sleep semi
+play 85
+sleep semi
+play 84
+sleep semi
+play 83
+sleep semi
+play 82
+sleep semi
+
+play 81
+sleep semi
+play 80
+sleep semi
+play 79
+sleep semi
+play 78
+sleep semi
+play 79
+sleep semi
+play 78
+sleep semi
+play 77
+sleep semi
+play 76
+sleep semi
+
+play 77
+sleep semi
+play 76
+sleep semi
+play 75
+sleep semi
+play 74
+sleep semi
+play 73
+sleep semi
+play 72
+sleep semi
+play 71
+sleep semi
+play 70
+sleep semi
+
+2.times do
+  play 69
+  sleep semi
+  play 68
+  sleep semi
+  play 67
+  sleep semi
+  play 66
+  sleep semi
+  play 67
+  sleep semi
+  play 66
+  sleep semi
+  play 65
+  sleep semi
+  play 64
+  sleep semi
+end
+# bar 7
+
+2.times do
+  2.times do
+    play 69
+    sleep semi
+    play 68
+    sleep semi
+    play 67
+    sleep semi
+    play 66
+    sleep semi
+    play 65
+    sleep semi
+    play 70
+    sleep semi
+    play 69
+    sleep semi
+    play 68
+    sleep semi
+
+    play 69
+    sleep semi
+    play 68
+    sleep semi
+    play 67
+    sleep semi
+    play 66
+    sleep semi
+    play 65
+    sleep semi
+    play 66
+    sleep semi
+    play 67
+    sleep semi
+    play 68
+    sleep semi
+  end
+
+    play 69
+    sleep semi
+    play 68
+    sleep semi
+    play 67
+    sleep semi
+    play 66
+    sleep semi
+    play 67
+    sleep semi
+    play 66
+    sleep semi
+    play 65
+    sleep semi
+    play 64
+    sleep semi
+
+    play 65
+    sleep semi
+    play 66
+    sleep semi
+    play 67
+    sleep semi
+    play 68
+    sleep semi
+    play 69
+    sleep semi
+    play 70
+    sleep semi
+    play 69
+    sleep semi
+    play 68
+    sleep semi
+
+    play 69
+    sleep semi
+    play 68
+    sleep semi
+    play 67
+    sleep semi
+    play 66
+    sleep semi
+    play 67
+    sleep semi
+    play 66
+    sleep semi
+    play 65
+    sleep semi
+    play 64
+    sleep semi
+
+    play 65
+    sleep semi
+    play 66
+    sleep semi
+    play 67
+    sleep semi
+    play 68
+    sleep semi
+    play 69
+    sleep semi
+    play 71
+    sleep semi
+    play 72
+    sleep semi
+    play 73
+    sleep semi
+
+  2.times do
+    play 74
+    sleep semi
+    play 73
+    sleep semi
+    play 72
+    sleep semi
+    play 71
+    sleep semi
+    play 70
+    sleep semi
+    play 75
+    sleep semi
+    play 74
+    sleep semi
+    play 73
+    sleep semi
+
+    play 74
+    sleep semi
+    play 73
+    sleep semi
+    play 72
+    sleep semi
+    play 71
+    sleep semi
+    play 70
+    sleep semi
+    play 71
+    sleep semi
+    play 72
+    sleep semi
+    play 73
+    sleep semi
+  end
+
+  2.times do
+    play 74
+    sleep semi
+    play 73
+    sleep semi
+    play 72
+    sleep semi
+    play 71
+    sleep semi
+    play 72
+    sleep semi
+    play 71
+    sleep semi
+    play 70
+    sleep semi
+    play 69
+    sleep semi
+
+    play 70
+    sleep semi
+    play 71
+    sleep semi
+    play 72
+    sleep semi
+    play 73
+    sleep semi
+    play 74
+    sleep semi
+    play 75
+    sleep semi
+    play 74
+    sleep semi
+    play 73
+    sleep semi
+  end
+
+  play 74
+  sleep semi
+  play 62
+  sleep semi
+  play 62
+  sleep semi
+  play 62
+  sleep semi
+  play 62
+  sleep semi
+  play 62
+  sleep semi
+  play 62
+  sleep semi
+  play 62
+  sleep semi
+
+  play 63 
+#  play 61
+  sleep semi
+  play 62
+  sleep semi
+  play 63
+#  play 61
+  sleep semi
+  play 75
+  sleep semi
+  play 63
+#  play 61
+  sleep semi
+  play 62
+  sleep semi
+  play 63
+#  play 61
+  sleep semi
+  play 75
+  sleep semi
+
+  8.times do
+    play 74 
+    sleep semi
+  end
+
+  play 75
+  sleep semi
+  play 74
+  sleep semi
+  play 75
+  sleep semi
+  play 87
+  sleep semi
+  play 75
+  sleep semi
+  play 74
+  sleep semi
+  play 75
+  sleep semi
+  play 87
+  sleep semi
+
+  play 86
+  sleep semi
+  play 75
+  sleep semi
+  play 74
+  sleep semi
+  play 73
+  sleep semi
+  play 74
+  sleep semi
+  play 75
+  sleep semi
+  play 74
+  sleep semi
+  play 73
+  sleep semi
+
+  2.times do
+    play 74
+    sleep semi
+    play 75
+    sleep semi
+    play 74
+    sleep semi
+    play 73
+    sleep semi
+  end
+
+  2.times do
+    play 74
+    sleep semi
+    play 75
+    sleep semi
+    play 76
+    sleep semi
+    play 77
+    sleep semi
+    play 78
+    sleep semi
+    play 77
+    sleep semi
+    play 76
+    sleep semi
+    play 75
+    sleep semi
+  end
+
+# bar 31
+  play 74
+  sleep semi
+  7.times do  
+    play 67
+    sleep semi
+  end
+
+  2.times do
+    play 68
+    sleep semi
+    play 67
+    sleep semi
+    play 68
+    sleep semi
+    play 80
+    sleep semi
+  end
+
+  play 67
+  sleep semi
+
+# bar 33
+  7.times do
+    play 79
+    sleep semi
+  end
+
+  2.times do
+    play 80
+    sleep semi
+    play 79
+    sleep semi
+    play 80
+    sleep semi
+    play 92
+    sleep semi
+  end
+
+  play 91
+  sleep semi
+  play 80
+  sleep semi
+  play 79
+  sleep semi
+  play 78
+  sleep semi
+  play 79
+  sleep semi
+  play 80
+  sleep semi
+  play 79
+  sleep semi
+  play 78
+  sleep semi
+
+# bar 36
+  2.times do
+    play 79
+    sleep semi
+    play 80
+    sleep semi
+    play 79
+    sleep semi
+    play 78
+    sleep semi
+  end
+
+  2.times do
+    play 79
+    sleep semi
+    play 80
+    sleep semi
+    play 81
+    sleep semi
+    play 82
+    sleep semi
+    play 83
+    sleep semi
+    play 82
+    sleep semi
+    play 81
+    sleep semi
+    play 80
+    sleep semi
+  end
+
+
+  play 79
+  sleep semi
+  play 78
+  sleep semi
+  play 77
+  sleep semi
+  play 76
+  sleep semi
+  play 75
+  sleep semi
+  play 80
+  sleep semi
+  play 79
+  sleep semi
+  play 78
+  sleep semi
+
+# bar 40
+  play 79
+  sleep semi
+  play 78
+  sleep semi
+  play 77
+  sleep semi
+  play 76
+  sleep semi
+  play 75
+  sleep semi
+  play 76
+  sleep semi
+  play 77
+  sleep semi
+  play 78
+  sleep semi
+
+  play 79
+  sleep semi
+  play 78
+  sleep semi
+  play 77
+  sleep semi
+  play 76
+  sleep semi
+  play 77
+  sleep semi
+  play 76
+  sleep semi
+  play 75
+  sleep semi
+  play 74
+  sleep semi
+
+#bar 42
+  play 75
+  sleep semi
+  play 76
+  sleep semi
+  play 77
+  sleep semi
+  play 78
+  sleep semi
+  play 77
+  sleep semi
+  play 78
+  sleep semi
+  play 79
+  sleep semi
+  play 80
+  sleep semi
+
+  play 81
+  sleep semi
+  play 80
+  sleep semi
+  play 79
+  sleep semi
+  play 78
+  sleep semi
+  play 79
+  sleep semi
+  play 78
+  sleep semi
+  play 77
+  sleep semi
+  play 76
+  sleep semi
+
+# bar 44
+  play 77
+  sleep semi
+  play 76
+  sleep semi
+  play 75
+  sleep semi
+  play 74
+  sleep semi
+  play 73
+  sleep semi
+  play 72
+  sleep semi
+  play 71
+  sleep semi
+  play 70
+  sleep semi
+
+  8.times do
+    play 69
+    sleep semi
+    play 70
+    sleep semi
+    play 69
+    sleep semi
+    play 68
+    sleep semi
+  end
+
+#bar 49
+  play 69
+  sleep semi
+  play 68
+  sleep semi
+  play 67
+  sleep semi
+  play 66
+  sleep semi
+  play 67
+  sleep semi
+  play 66
+  sleep semi
+  play 65
+  sleep semi
+  play 64
+  sleep semi
+
+# bar 50
+  play 65
+  sleep semi
+  play 64
+  sleep semi
+  play 63
+  sleep semi
+  play 62
+  sleep semi
+  play 61
+  sleep semi
+  play 60
+  sleep semi
+  play 59
+  sleep semi
+  play 58
+  sleep semi
+
+  8.times do
+    play 57
+    sleep semi
+    play 58
+    sleep semi
+    play 57
+    sleep semi
+    play 56
+    sleep semi
+  end
+
+  play 57
+  sleep semi
+  play 58
+  sleep semi
+  play 59
+  sleep semi
+  play 60
+  sleep semi
+  play 61
+  sleep semi
+  play 62
+  sleep semi
+  play 63
+  sleep semi
+  play 64
+  sleep semi
+
+  play 65
+  sleep semi
+  play 66
+  sleep semi
+  play 67
+  sleep semi
+  play 68
+  sleep semi
+  play 69
+  sleep semi
+  play 70
+  sleep semi
+  play 71
+  sleep semi
+  play 72
+  sleep semi
+
+  play 73
+  sleep semi
+  play 74
+  sleep semi
+  play 75
+  sleep semi
+  play 76
+  sleep semi
+  play 77
+  sleep semi
+  play 78
+  sleep semi
+  play 79
+  sleep semi
+  play 80
+  sleep semi
+
+  2.times do
+    play 81
+    sleep semi
+    play 82
+    sleep semi
+    play 81
+    sleep semi
+    play 80
+    sleep semi
+  end
+end # main repeat loop
+
+# bar 63
+2.times do
+  play 81
+  sleep semi
+  play 80
+  sleep semi
+  play 79
+  sleep semi
+  play 78
+  sleep semi
+  play 77
+  sleep semi
+  play 82
+  sleep semi
+  play 81
+  sleep semi
+  play 80
+  sleep semi
+
+  play 81
+  sleep semi
+  play 80
+  sleep semi
+  play 79
+  sleep semi
+  play 78
+  sleep semi
+  play 77
+  sleep semi
+  play 78
+  sleep semi
+  play 79
+  sleep semi
+  play 80
+  sleep semi
+end
+
+# bar 65, page 4
+play 81
+sleep semi
+play 80
+sleep semi
+play 79
+sleep semi
+play 78
+sleep semi
+play 79
+sleep semi
+play 78
+sleep semi
+play 77
+sleep semi
+play 76
+sleep semi
+
+play 77
+sleep semi
+play 78
+sleep semi
+play 79
+sleep semi
+play 80
+sleep semi
+play 81
+sleep semi
+play 83
+sleep semi
+play 84
+sleep semi
+play 85
+sleep semi
+
+#bar 67
+2.times do
+  play 86
+  sleep semi
+  play 85
+  sleep semi
+  play 84
+  sleep semi
+  play 83
+  sleep semi
+  play 82
+  sleep semi
+  play 87
+  sleep semi
+  play 86
+  sleep semi
+  play 85
+  sleep semi
+
+  play 86
+  sleep semi
+  play 85
+  sleep semi
+  play 84
+  sleep semi
+  play 83
+  sleep semi
+  play 82
+  sleep semi
+  play 83
+  sleep semi
+  play 84
+  sleep semi
+  play 85
+  sleep semi
+end
+
+# bar 71
+
+play 86
+sleep semi
+play 85
+sleep semi
+play 84
+sleep semi
+play 83
+sleep semi
+play 84
+sleep semi
+play 83
+sleep semi
+play 82
+sleep semi
+play 81
+sleep semi
+
+play 82
+sleep semi
+play 83
+sleep semi
+play 84
+sleep semi
+play 85
+sleep semi
+play 86
+sleep semi
+play 87
+sleep semi
+play 86
+sleep semi
+play 85
+sleep semi
+
+play 86
+sleep semi
+play 85
+sleep semi
+play 84
+sleep semi
+play 83
+sleep semi
+play 82
+sleep semi
+play 83
+sleep semi
+play 84
+sleep semi
+play 85
+sleep semi
+
+# bar 74
+play 74
+sleep semi
+play 76
+sleep semi
+play 77
+sleep semi
+play 79
+sleep semi
+play 81
+sleep semi
+play 83
+sleep semi
+play 81
+sleep semi
+play 80
+sleep semi
+
+# bar 75
+2.times do
+  play 81
+  sleep semi
+  play 80
+  sleep semi
+  play 79
+  sleep semi
+  play 78
+  sleep semi
+  play 77
+  sleep semi
+  play 82
+  sleep semi
+  play 81
+  sleep semi
+  play 80
+  sleep semi
+
+  play 81
+  sleep semi
+  play 80
+  sleep semi
+  play 79
+  sleep semi
+  play 78
+  sleep semi
+  play 77
+  sleep semi
+  play 78
+  sleep semi
+  play 79
+  sleep semi
+  play 80
+  sleep semi
+end
+
+#bar 79
+play 81
+sleep quaver
+play 73
+sleep semi
+play 74
+sleep semi
+play 75
+sleep semi
+play 76
+sleep semi
+play 77
+sleep semi
+play 78
+sleep semi
+
+play 79
+sleep semi
+play 78
+sleep semi
+play 77
+sleep semi
+play 76
+sleep semi
+play 77
+sleep semi
+play 76
+sleep semi
+play 75
+sleep semi
+play 74
+sleep semi
+
+# bar 81
+play 73
+sleep semi
+play 74
+sleep semi
+play 75
+sleep semi
+play 76
+sleep semi
+play 77
+sleep semi
+play 78
+sleep semi
+play 79
+sleep semi
+play 80
+sleep semi
+
+2.times do
+  play 81
+  sleep semi
+  play 82
+  sleep semi
+  play 81
+  sleep semi
+  play 80
+  sleep semi
+end
+
+# bar 83
+play 93
+sleep quaver
+play 73
+sleep semi
+play 74
+sleep semi
+play 75
+sleep semi
+play 76
+sleep semi
+play 77
+sleep semi
+play 78
+sleep semi
+
+play 79
+sleep semi
+play 78
+sleep semi
+play 77
+sleep semi
+play 76
+sleep semi
+play 77
+sleep semi
+play 76
+sleep semi
+play 75
+sleep semi
+play 74
+sleep semi
+
+# bar 85
+play 73
+sleep semi
+play 74
+sleep semi
+play 75
+sleep semi
+play 76
+sleep semi
+play 77
+sleep semi
+play 78
+sleep semi
+play 79
+sleep semi
+play 80
+sleep semi
+
+play 81
+sleep semi
+play 82
+sleep semi
+play 81
+sleep semi
+play 80
+sleep semi
+play 81
+sleep semi
+play 83
+sleep semi
+play 84
+sleep semi
+play 85
+sleep semi
+
+play 86
+sleep semi
+play 85
+sleep semi
+play 84
+sleep semi
+play 83
+sleep semi
+play 84
+sleep semi
+play 83
+sleep semi
+play 82
+sleep semi
+play 81
+sleep semi
+
+play 82
+sleep semi
+play 81
+sleep semi
+play 80
+sleep semi
+play 79
+sleep semi
+play 78
+sleep semi
+play 77
+sleep semi
+play 76
+sleep semi
+play 75
+sleep semi
+
+#bar 89
+play 74
+sleep semi
+play 73
+sleep semi
+play 72
+sleep semi
+play 71
+sleep semi
+play 72
+sleep semi
+play 71
+sleep semi
+play 70
+sleep semi
+play 69
+sleep semi
+
+play 70
+sleep semi
+play 69
+sleep semi
+play 68
+sleep semi
+play 67
+sleep semi
+play 66
+sleep semi
+play 65
+sleep semi
+play 64
+sleep semi
+play 63
+
+#bar 91
+play 62
+sleep semi
+play 63
+sleep semi
+play 62
+sleep semi
+play 61
+sleep semi
+play 63
+sleep semi
+play 62
+sleep semi
+play 63
+sleep semi
+play 75
+sleep semi
+
+play 62
+sleep semi
+play 63
+sleep semi
+play 62
+sleep semi
+play 64
+sleep semi
+play 62
+sleep semi
+play 65
+sleep semi
+play 62
+sleep semi
+play 67
+sleep semi
+
+# 93
+play 81
+sleep semi
+play 82
+sleep semi
+play 81
+sleep semi
+play 80
+sleep semi
+play 81
+sleep semi
+play 93
+sleep semi
+play 81
+sleep semi
+play 95
+sleep semi
+
+play 93
+sleep semi
+play 83
+sleep semi
+play 81
+sleep semi
+play 95
+sleep semi
+play 81
+sleep semi
+play 96
+sleep semi
+play 81
+sleep semi
+play 97
+sleep semi
+
+# 95
+play 98
+sleep quaver
+# need a quaver rest here but dunno how
+sleep quaver
+play 69
+sleep semi
+play 70
+sleep semi
+play 71
+sleep semi
+play 72
+sleep semi
+
+play 73
+sleep semi
+play 74
+sleep semi
+play 75
+sleep semi
+play 76
+sleep semi
+play 77
+sleep semi
+play 78
+sleep semi
+play 79
+sleep semi
+play 80
+sleep semi
+
+#97
+play 81
+sleep semi
+play 82
+sleep semi
+play 83
+sleep semi
+play 84
+sleep semi
+play 85
+sleep semi
+play 86
+sleep semi
+play 87
+sleep semi
+play 88
+sleep semi
+
+play 89
+sleep semi
+play 90
+sleep semi
+play 91
+sleep semi
+play 92
+sleep semi
+play 93
+sleep semi
+play 95
+sleep semi
+play 96
+sleep semi
+play 97
+sleep semi
+
+#99
+play 98 
+#play 93
+#play 88
+sleep minim
+
+#100
+#play 98
+#play 101
+#play 105
+play 110
+sleep minim
+
+play 86
+sleep crotchet
+
---
+++ planetClaireIsh.rb
@@ -0,0 +1,6 @@
+ptch = 12
+5.times do
+  sample :ambi_glass_rub, pitch: ptch
+  ptch = ptch -1
+  sleep 2.5
+end
