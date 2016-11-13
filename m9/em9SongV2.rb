a87befc99761b08e8c269399c9b3c8e49dee7d6a
juanpickselov@sbcglobal.net <juanpickselov@sbcglobal.net>
2016-11-13 15:31:23 -0500

    created m9 folder for continuing The Song!

---
+++ m9/em9SongV2.rb
@@ -0,0 +1,45 @@
+define :guitEm9NoFx do
+  sample :guit_em9
+  sleep 3
+end
+
+2.times do
+  3.times do
+    guitEm9NoFx
+  end
+  sample :guit_em9, attack: 2,beat_stretch:12
+  sleep 3
+  sample :guit_em9, attack: 1,beat_stretch:12
+  sleep 3
+  sample :guit_em9, attack:2, beat_stretch:18
+  sleep 3
+end
+
+2.times do
+  with_fx :flanger do
+    3.times do
+      sample :guit_e_fifths
+      sleep 1.753
+    end
+  end
+  
+  with_fx :flanger do
+    3.times do
+      sample :guit_e_fifths, pitch: -1
+      sleep 1.753
+    end
+  end
+  sleep 1
+end
+
+2.times do
+  sample :guit_em9, attack: 2,beat_stretch:12
+  sleep 3
+  sample :guit_em9, attack: 1,beat_stretch:12
+  sleep 3
+  sample :guit_em9, attack:2, beat_stretch:18
+  sleep 2
+  3.times do
+    guitEm9NoFx
+  end
+end
