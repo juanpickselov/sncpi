c02c3a1bad01f741140f6ac1ff8a001bd52d8bc4
juanpickselov@sbcglobal.net <juanpickselov@sbcglobal.net>
2016-11-15 20:23:52 -0500

    Where do I begin...

---
+++ whereDoIBegin.rb
@@ -0,0 +1,24 @@
+with_fx :reverb do
+  with_synth :piano do
+    play :c5,slide: 2
+    sleep 0.25
+    play :e4
+    sleep 0.25
+    play :e4
+    sleep 0.25
+    play :c5
+    sleep 0.25
+    play :c5
+    sleep 0.75
+    play :e4
+    sleep 0.25
+    play :e4
+    sleep 0.25
+    play :c5
+    sleep 0.25
+    play :c5
+    sleep 0.25
+    play :e4
+    sleep 0.25
+  end
+end
