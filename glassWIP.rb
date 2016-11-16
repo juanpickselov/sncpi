b45d88672b8528898735e27faf7f6bfc9aebc879
juanpickselov@sbcglobal.net <juanpickselov@sbcglobal.net>
2016-11-15 20:26:13 -0500

    glass wip

---
+++ glassWIP.rb
@@ -0,0 +1,35 @@
+define :pno1 do
+  with_fx :reverb do
+    with_synth :piano do
+      play_chord chord(:g4, :major)
+    end
+    sleep 0.15
+    with_synth :piano do
+      play_chord chord(:f4, :major)
+    end
+    sleep 0.15
+  end
+end
+
+define :pno2 do
+  with_fx :reverb do
+    with_synth :piano do
+      play_chord chord(:c4, :major)
+    end
+    sleep 0.15
+    with_synth :piano do
+      play_chord chord(:d4, :major)
+    end
+    sleep 0.15
+  end
+end
+
+8.times do
+  pno1
+end
+
+8.times do
+  pno2
+end
+with_synth :piano do
+end
