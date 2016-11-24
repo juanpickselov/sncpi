7aaa550efee197ed012ed76572545128be1d425c
juanpickselov@sbcglobal.net <juanpickselov@sbcglobal.net>
2016-11-23 20:54:56 -0500

    messing with samples and settings

---
+++ emperor.rb
@@ -0,0 +1,12 @@
+2.times do
+  sample :ambi_dark_woosh, pitch: 10
+  sleep 1.1
+  sample :ambi_dark_woosh, pitch: 8
+  sleep 1.1
+  sample :ambi_dark_woosh, pitch: 10
+  sleep 1.1
+  sample :ambi_dark_woosh, pitch: 6
+  sleep 1.1
+  sample :ambi_dark_woosh, pitch: 6.5
+  sleep 1.5
+end
