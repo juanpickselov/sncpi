705e7c1470578f7a11c8ea56d59f17b7e0f5f1e8
juanpickselov@sbcglobal.net <juanpickselov@sbcglobal.net>
2016-11-23 20:48:18 -0500

    messing with samples and settings

---
+++ oweeo.rb
@@ -0,0 +1,19 @@
+1.times do
+  sample :ambi_dark_woosh, pitch: 8
+  sleep 1
+  sample :ambi_dark_woosh, pitch: 6
+  sleep 1
+  sample :ambi_dark_woosh, pitch: 8
+  sleep 2
+end
+
+1.times do
+  sample :ambi_dark_woosh, pitch: 10
+  sleep 1.2
+  sample :ambi_dark_woosh, pitch: 6
+  sleep 1.2
+  sample :ambi_dark_woosh, pitch: 10
+  sleep 2
+end
+
+
